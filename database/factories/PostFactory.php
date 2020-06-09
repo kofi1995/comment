56f14d70ca4c5a872db630970e4bd96965a18037
Kofi Kwarteng <kofi@kofidev.com>
2020-06-09 14:08:53 -0400

    Include vue.js scaffolding

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,11 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Models\DB\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->paragraph(rand(4, 10)),
+        'message' => implode('<br />', $faker->paragraphs(rand(3, 6))),
     ];
 });
